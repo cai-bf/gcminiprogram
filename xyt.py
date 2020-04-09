93b1ba74f7b16534c51b41571db545d9719cba45
KanoLin <fordreams@outlook.com>
2020-04-09 15:52:57 +0800

    update init school table command

--- xyt.py
+++ xyt.py
@@ -30,6 +30,42 @@ def init_token():
     token = encode_auth_token(u2.id, 120)
     print('学生token: {}'.format(token))
 
+@app.cli.command('init_school')
+def init_school():
+    school_list = [
+        '机械与汽车工程学院',
+        '建筑学院',
+        '土木与交通学院',
+        '电力学院',
+        '电子与信息学院',
+        '自动化科学与工程学院',
+        '材料科学与工程学院',
+        '化学与化工学院',
+        '轻工科学与工程学院',
+        '食品科学与工程学院',
+        '数学学院',
+        '物理与光电学院',
+        '工商管理学院',
+        '公共管理学院',
+        '外国语学院',
+        '体育学院',
+        '马克思主义学院',
+        '计算机科学与工程学院',
+        '软件学院',
+        '环境与能源学院',
+        '生物科学与工程学院',
+        '新闻与传播学院',
+        '设计学院',
+        '法学院',
+        '经济与贸易学院',
+        '艺术学院',
+        '医学院'
+    ]
+    for name in school_list:
+        s = School(name=name)
+        db.session.add(s)
+    db.session.commit()
+    print('学院信息导入完毕')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=10000)
